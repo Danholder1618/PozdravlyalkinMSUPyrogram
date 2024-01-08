97ac2f6d1d507dcf8a70bb8725ac61ed4d855c96
Danil <Danholder2003@yandex.com>
2024-01-08 18:56:14 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+BOT_TOKEN = '6688011409:AAEXwQMxREESQdyRTezUx4BP9D0FH6Ir0KI'
+CHAT_ID = '514749621'
+PIC_PLACE = [
+    "pics/Pic1.png",
+    "pics/Pic2.png",
+    "pics/Pic3.png",
+    "pics/Pic4.png",
+    "pics/Pic5.png",
+    "pics/Pic6.png",
+    "pics/Pic7.png",
+    "pics/Pic8.png",
+    "pics/Pic9.png",
+    "pics/Pic10.png",
+    "pics/Pic11.png",
+    "pics/Pic12.png",
+]
+LOGO_PLACE = "pics/logo.jpg"
+APP_API_ID = '28348962'
+APP_API_HASH = 'e61e433746223011517e15f513172617'
+BG_PATH = "temp.png"
