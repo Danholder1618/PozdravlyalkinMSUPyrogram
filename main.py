7b2b61446188a85ec4a8478d4bf4a7ff10832999
Danil <Danholder2003@yandex.com>
2024-02-21 01:43:36 +0500

    фигня

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
-import schedule
-import time
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import config
 import db_functions
 import utils
@@ -14,51 +13,46 @@ bot_token = config.BOT_TEST_TOKEN
 group_id = config.TEST_CHANEL
 chat_id = config.MY_ID
 
-async def main():
-    app = Client("Поздравлялкин", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("Поздравлялкин", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+async def send_congratulation():
+    iteration = 0
+    now = datetime.now()
+    current_date = now.strftime("%d.%m")
+    bd_today = db_functions.name_and_group_get(current_date)
+    last_congratulation_sent = await utils.get_congratulation_status()
+
+    await app.send_message(chat_id, f"Статус {'Отправлено' if last_congratulation_sent == 1 else 'Не Отправлено'}")
+
+    if bd_today and not last_congratulation_sent:
+        await app.send_message(chat_id, "Сегодня кто-то родился, поздравление отправляется")
+        num_images = -(-len(bd_today) // 3)
+        for i in range(num_images):
+            start_idx = i * 3
+            end_idx = min((i + 1) * 3, len(bd_today))
+            people_to_congratulate = bd_today[start_idx:end_idx]
+
+            await birthday(app, group_id, chat_id, people_to_congratulate, iteration)
+            iteration += 1
+            await utils.save_congratulation_status(1)
+    else:
+        await app.send_message(chat_id, "Сегодня никто не родился (либо это перезапуск), поздравление не отправляется")
+
+async def scheduler():
     await app.start()
     await app.send_message(chat_id, "Бот начал работу")
 
-    async def send_congratulation():
-        iteration = 0
-        now = datetime.now()
-        current_date = now.strftime("%d.%m")
-        current_date = "'" + current_date + "'"
-        bd_today = db_functions.name_and_group_get(current_date)
-        last_congratulation_sent = await utils.get_congratulation_status()
-
-        await app.send_message(chat_id, f"Статус {'Отправлено' if last_congratulation_sent == 1 else 'Не Отправлено'}")
-
-        if bd_today and not last_congratulation_sent:
-            await app.send_message(chat_id, "Сегодня кто-то родился, поздравление отправляется")
-
-            # Кол-во разбиейний людей
-            num_images = -(-len(bd_today) // 3)
-
-            # Создаем и отправляем каждую картинку
-            for i in range(num_images):
-                start_idx = i * 3
-                end_idx = min((i + 1) * 3, len(bd_today))
-                people_to_congratulate = bd_today[start_idx:end_idx]
-
-                await birthday(app, group_id, chat_id, people_to_congratulate, iteration)
-                iteration += 1
-                await utils.save_congratulation_status(1)
-        else:
-            await app.send_message(chat_id,
-                                   "Сегодня никто не родился (либо это перезапуск), поздравление не отправляется")
-
-    # Проверяем, была ли отправлена картинка сегодня перед входом в цикл
     last_congratulation_sent = await utils.get_congratulation_status()
     if not last_congratulation_sent:
         await send_congratulation()
 
-    # Запланируем отправку поздравления каждый день в 8 утра
-    schedule.every().day.at("00:10").do(send_congratulation)
+    aioschedule.every().day.at("01:32").do(send_congratulation())
 
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(scheduler())
